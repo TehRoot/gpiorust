61ad27078f52177aef0048768bdaab4c30bcc6e5
Mark Cardish <rewt@Marks-MacBook-Pro.local>
2018-03-12 10:09:57 -0400

    changes

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn interrupt(pin: u64) -> sysfs_gpio::Result<()> {
 		    		vecstore.clear();
 			} if urandom.len() == 8 {
 		    	for x in urandom.iter() {
-		    		print!("{}", x); 
+		    		println!("{}", x); 
 		    		decimal = decimal * 2 + x;
 		    		if decimal < 128 && decimal > 32 {
 		    			//println!("Conversion: {}", decimal as char);
