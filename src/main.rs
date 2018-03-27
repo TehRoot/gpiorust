51a125d76f4556ba6d50933197f34e124e631ae4
Mark Cardish <rewt@Marks-MacBook-Pro.local>
2018-03-27 19:05:42 -0400

    more experimental changes

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,6 @@ fn interrupt(pin: u64) -> sysfs_gpio::Result<()> {
     let mut x:i64 = 0;
     let mut y:i64 = 0;
     let mut z:i64 = 0;
-    //let mut difference1:i64 = 0;
-    //let mut difference2:i64 = 0;
     let mut count:i64 = 0;
     let mut urandom: Vec<u8> = vec![];
     let mut decimal:u8 = 0;
@@ -36,18 +34,11 @@ fn interrupt(pin: u64) -> sysfs_gpio::Result<()> {
 		    		x = vecstore[1];
 		    		y = vecstore[2];
 		    		z = vecstore[3];
-		    		//difference1 = x - w;
-		    		//difference2 = z - y;
-		    		//if difference1 < difference2 {
-		    		//	urandom.push(0);
-		    		//} else if difference2 < difference1 {
-		    		//	urandom.push(1);
-		    		//}
 		    		urandom.push(swap_bits(w, x, y, z));
 		    		vecstore.clear();
 			} if urandom.len() == 8 {
 		    	for x in urandom.iter() {
-		    		print!("{}", x); 
+		    		//print!("{}", x); 
 		    		io::stdout().flush().unwrap();
 		    		decimal = decimal * 2 + x;
 		    		if decimal < 128 && decimal > 32 {
@@ -76,6 +67,7 @@ fn swap_bits(time1: i64, time2: i64, time3: i64, time4: i64) -> u8 {
 	if difference1 != difference2 {
 		flipper ^= true;
 		flipper ^ (difference1 > difference2);
+		println!("{}", flipper);
 		return flipper as u8;
 	} else {
 		return 0;
